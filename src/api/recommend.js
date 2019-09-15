23d05ead30f048182045d8bbfd29d4f23f34691d
翔君 <1733878114@qq.com>
2019-09-15 16:43:55 +0800

    test
    test

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -38,7 +38,7 @@ export function getDiscList() {
 
 export function getSongList(disstid) {
   const url = '/api/SongList'
-
+  console.log('111')
   const data = Object.assign({}, commonParams, {
     type: 1,
     json: 1,
